0ea24ae59a072b3841cdb426c23095cab2d76cd8
alanleew <alanleew412@gmail.com>
2025-04-02 21:06:38 -0700

    Drafting layout for class definition

---
+++ main.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+class InstaFollower:
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+
+
+    def login(self):
+        pass
+
+    def find_followers(self):
+        pass
+
+    def follow(self):
+        pass
+
+bot = InstaFollower()
+bot.login()
