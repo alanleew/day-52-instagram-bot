cd8cd69159e27292cb2d418e39444934c9c7e443
alanleew <alanleew412@gmail.com>
2025-04-04 16:57:07 -0700

    Put project on pause since it can cause account to be banned due to bot activity.

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common import NoSuchElementException
 from os import getenv
 from time import sleep
 
 USERNAME = getenv('USERNAME')
 PASSWORD = getenv('PASSWORD')
+SEARCH_QUERY = "Apple"
 
 class InstaFollower:
     def __init__(self):
@@ -22,14 +24,23 @@ class InstaFollower:
         sleep(2)
         self.login_button = self.driver.find_element(By.XPATH, '//*[@id="loginForm"]/div[1]/div[3]/button')
         self.login_button.click()
-        #TODO - set conditional so that user can proceed if 2FA is prompted
-        input('Did you get past the 2FA? Enter return to continue')
+        sleep(5)
+        self.not_now_button = self.driver.find_element(By.XPATH, '//div[contains(text(), "Not now")]')
+        self.not_now_button.click()
+
 
     def find_followers(self):
-        pass
+        sleep(5)
+        self.search_button = self.driver.find_element(By.XPATH, '//svg[contains(text(), "Search")]')
+        self.search_button.send_keys(SEARCH_QUERY)
+        sleep(2)
+        self.apple_account = self.driver.find_element(By.CSS_SELECTOR, "a[contains(text(), 'Apple')]")
+        self.apple_account.click()
+
 
     def follow(self):
-        pass
+        ...
 
 bot = InstaFollower()
 bot.login()
+bot.find_followers()
